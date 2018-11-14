1ccdedb4a640b68f40e1ca9b62d7dbb251750adb
shelzTimesignature <sheltontembo@gmail.com>
2018-11-14 20:20:59 +0200

    first website upload

---
+++ precache-manifest.f5ec6070073b95e2dac6c55a532cfa1c.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "518e2b2d263982d2caa1e6514b4b4eac",
+    "url": "/font-awesome/less/stacked.less"
+  },
+  {
+    "revision": "950bd7235543297ba59c19615cacf683",
+    "url": "/main.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "71df44ca1c702fefd1b27c961d7ef622",
+    "url": "/font-awesome/package.json"
+  },
+  {
+    "revision": "b083e654143d6caf9e86a4001a0c6325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48a816e4624dc5da6e59",
+    "url": "/js/chunk-vendors.a7a450a8.js"
+  },
+  {
+    "revision": "28c8c1aba835805c816d",
+    "url": "/js/app.34ae2e24.js"
+  },
+  {
+    "revision": "39ff4f359a7b81d6585075715f41e5dc",
+    "url": "/font-awesome/scss/_animated.scss"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/font-awesome/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/font-awesome/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "aeab411036f3d5ef4c8880e59ce15355",
+    "url": "/img/log.jpg"
+  },
+  {
+    "revision": "08baef05e05301cabc91599a54921081",
+    "url": "/font-awesome/less/animated.less"
+  },
+  {
+    "revision": "fb4efe4ae63737706875bbbfc7b7e9af",
+    "url": "/font-awesome/less/core.less"
+  },
+  {
+    "revision": "1a23f38e9e1f413a23bacb00283f0488",
+    "url": "/img/c.jpg"
+  },
+  {
+    "revision": "5e07ec001f8d21bd279c12ee542813f7",
+    "url": "/font-awesome/less/fixed-width.less"
+  },
+  {
+    "revision": "898f90e40876883214bbd121b0c20e9f",
+    "url": "/font-awesome/less/bordered-pulled.less"
+  },
+  {
+    "revision": "15cb7faa02437c2f9719351c157fe7e7",
+    "url": "/font-awesome/less/font-awesome.less"
+  },
+  {
+    "revision": "8cb65280c0f889daf72626c21a7c8628",
+    "url": "/font-awesome/less/larger.less"
+  },
+  {
+    "revision": "975571323cf880a4a30601998236b027",
+    "url": "/font-awesome/less/list.less"
+  },
+  {
+    "revision": "fbb1f2f1ab96ba020c7f14208aac72b8",
+    "url": "/font-awesome/less/mixins.less"
+  },
+  {
+    "revision": "bf95b901c36b646ff457379bdcda94b7",
+    "url": "/font-awesome/less/icons.less"
+  },
+  {
+    "revision": "a8c41460c42a4fe9e98550f00c8b3f19",
+    "url": "/font-awesome/less/path.less"
+  },
+  {
+    "revision": "a8476cdc50c264abd11ff59d6a9dd025",
+    "url": "/font-awesome/less/rotated-flipped.less"
+  },
+  {
+    "revision": "0f881617264587bef0df6ce92253ecea",
+    "url": "/font-awesome/less/screen-reader.less"
+  },
+  {
+    "revision": "dc5261f37a8a01feeb52a746d16c0459",
+    "url": "/font-awesome/scss/_variables.scss"
+  },
+  {
+    "revision": "8c015559216d1654630a839b61c6b83d",
+    "url": "/font-awesome/scss/font-awesome.scss"
+  },
+  {
+    "revision": "be3f6eed38aa909483e1bd9ee0876e80",
+    "url": "/font-awesome/less/variables.less"
+  },
+  {
+    "revision": "5594237226aedfbca2fa1c7f4604c214",
+    "url": "/font-awesome/scss/_stacked.scss"
+  },
+  {
+    "revision": "8907bd7dbf4799e8120bda5568d76fea",
+    "url": "/font-awesome/scss/_screen-reader.scss"
+  },
+  {
+    "revision": "4cad0df17bf40327feae33fa9a6c6ba2",
+    "url": "/font-awesome/scss/_bordered-pulled.scss"
+  },
+  {
+    "revision": "ef059a98cf9de6ca5b77ee6850771cf0",
+    "url": "/font-awesome/scss/_core.scss"
+  },
+  {
+    "revision": "9277ab6964a434d499873687b00be906",
+    "url": "/font-awesome/scss/_fixed-width.scss"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/font-awesome/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "e95931566f6fc6ad5685c4fa9802e206",
+    "url": "/font-awesome/scss/_larger.scss"
+  },
+  {
+    "revision": "7107e80b053928271d5fcf422dc29490",
+    "url": "/font-awesome/scss/_list.scss"
+  },
+  {
+    "revision": "aa2b8f32b403733713d8885f14ab86cc",
+    "url": "/font-awesome/scss/_mixins.scss"
+  },
+  {
+    "revision": "ab5a9e8388563e097b5ce835601f01d2",
+    "url": "/font-awesome/scss/_path.scss"
+  },
+  {
+    "revision": "9f5d4bc6fadea89328d2aac26574a9d8",
+    "url": "/font-awesome/scss/_rotated-flipped.scss"
+  },
+  {
+    "revision": "de9fa842ad0b619a95ac4f42ac6ba930",
+    "url": "/font-awesome/scss/_icons.scss"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/font-awesome/fonts/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/font-awesome/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "0d2717cd5d853e5c765ca032dfd41a4d",
+    "url": "/font-awesome/fonts/FontAwesome.otf"
+  },
+  {
+    "revision": "269550530cc127b6aa5a35925a7de6ce",
+    "url": "/font-awesome/css/font-awesome.min.css"
+  },
+  {
+    "revision": "c495654869785bc3df60216616814ad1",
+    "url": "/font-awesome/css/font-awesome.css"
+  },
+  {
+    "revision": "4696fc50054238d2233e46546ba3383a",
+    "url": "/font-awesome/README.md"
+  },
+  {
+    "revision": "a1e5be58e81e919ba2e579cd1c65283e",
+    "url": "/font-awesome/HELP-US-OUT.txt"
+  },
+  {
+    "revision": "219367c6619ae20841c69a1a08b77bd6",
+    "url": "/font-awesome/.npmignore"
+  },
+  {
+    "revision": "28c8c1aba835805c816d",
+    "url": "/css/app.60108580.css"
+  }
+];
